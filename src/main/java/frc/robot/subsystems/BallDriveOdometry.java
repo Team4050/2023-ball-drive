4b751ea50be6b7af5f23d57ffbe1a6ef1e72ddf7
Kiefer <agentaston967st@gmail.com>
2023-09-30 15:43:52 -0400

    Odometry changes

--- src/main/java/frc/robot/subsystems/BallDriveOdometry.java
+++ src/main/java/frc/robot/subsystems/BallDriveOdometry.java
@@ -1,6 +1,12 @@
 package frc.robot.subsystems;
 
+import edu.wpi.first.math.estimator.SwerveDrivePoseEstimator;
 import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.math.geometry.Translation2d;
+import edu.wpi.first.math.kinematics.SwerveDriveKinematics;
+import edu.wpi.first.math.kinematics.SwerveDriveOdometry;
+import edu.wpi.first.math.kinematics.SwerveModulePosition;
 import edu.wpi.first.wpilibj.ADIS16448_IMU;
 import frc.robot.hazard.HazardVision;
 
@@ -8,11 +14,17 @@ public class BallDriveOdometry {
   private BallDriveSubsystem drivetrain;
   private ADIS16448_IMU imu;
   private HazardVision vision;
+  /** The kinematics are the same for swerve drive */
+  private SwerveDriveKinematics kinematics;
+  private SwerveDrivePoseEstimator estimator;
+  private Pose2d previous = new Pose2d();
 
   public BallDriveOdometry(BallDriveSubsystem drivetrain, ADIS16448_IMU imu, HazardVision vision) {
     this.drivetrain = drivetrain;
     this.imu = imu;
     this.vision = vision;
+    kinematics = new SwerveDriveKinematics(null);
+    estimator = new SwerveDrivePoseEstimator(kinematics, null, null, previous, null, null);
   }
 
   /**
@@ -20,9 +32,22 @@ public class BallDriveOdometry {
    * @return The robot's estimated pose
    */
   public Pose2d update() {
-    return null;
+    Translation2d[] measuredVectors = drivetrain.get();
+    Translation2d[] projectedVectors = new Translation2d[4];
+    SwerveModulePosition[] positions = new SwerveModulePosition[4];
+    positions[0] = new SwerveModulePosition(0, new Rotation2d(measuredVectors[0].getX(), measuredVectors[0].getY()));
+    if (vision.isTrustworthy()) {
+      estimator.addVisionMeasurement(vision.getEstimatedGlobalPose().get().estimatedPose.toPose2d(), 0);
+    }
+    previous = estimator.update(new Rotation2d(imu.getAngle()), positions);
+    return previous;
+  }
 
-
-    /* TODO: figure out odometry again :\ */
+  /**
+   * Returns the imu.getAngle() but in a Rotation2d.
+   * @return
+   */
+  public Rotation2d getAngle() {
+    return new Rotation2d(Math.toRadians(imu.getAngle()));
   }
 }
